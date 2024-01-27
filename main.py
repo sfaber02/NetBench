1439c3833ca4e10e420c84978aacdd3686269b4b
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2024-01-27 00:53:32 -0500

    live graphing working

--- main.py
+++ main.py
@@ -4,34 +4,59 @@ import iperf3
 import subprocess
 from dotenv import load_dotenv
 import os
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import curdoc, figure
 
 HOST = os.getenv("IPERF_SERVER_ADDRESS")
 PORT = os.getenv("IPERF_PORT")
 
+# Create column data source for plot
+source = ColumnDataSource(dict(x=[], y=[]))
+#Create plot
+p = figure(title="Network Speed vs. Time", x_axis_label="Time in Seconds", y_axis_label="Mbits / sec")
+p.line(x="x", y="y", source=source)
+
+#store subprocess
+process = None
+count = 0
 
 def main():
     load_dotenv()
     print(f"Testing on host {HOST}, port {PORT}")
 
-    # plot()
+    curdoc().add_root(p)
+    curdoc().add_periodic_callback(update, 100)
 
-    command = [
-        "iperf3",
-        "-c",
-        HOST,
-        "-p",
-        PORT,
-        "-i",
-        "0.5",
-        "-t",
-        "10",
-        "--forceflush"
-    ]
 
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True)
+def update():
+    global process
+    global count
+    try: 
+        if process is None:
+            command = [
+                "iperf3",
+                "-c",
+                HOST,
+                "-p",
+                PORT,
+                "-i",
+                "0.5",
+                "-t",
+                "10",
+                "--forceflush"
+            ]
+            process = subprocess.Popen(command, stdout=subprocess.PIPE)
+        line = process.stdout.readline().decode().strip()
+        #parse data here
+        print(line)
+        print (f"count = {count}")
+        data_x = count
+        data_y = count
+        count += 1
+        source.stream(dict(x=[data_x], y=[data_y]))
+    except Exception as e:
+        print (f"Exception {e}")
 
-    for line in process.stdout:
-        print(line.strip())
 
 
 main()
