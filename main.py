5677ea232570175b618e08245c963cdf0df9cc21
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2024-01-26 23:40:49 -0500

    ditched iperf python lib in favor of leveraging iperf CLI

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 from iperf import Iperf_Client
+import iperf3
+import subprocess
 from dotenv import load_dotenv
 import os
-    
-HOST = os.getenv('IPERF_SERVER_ADDRESS')
-PORT = int(os.getenv('IPERF_PORT'))
+
+HOST = os.getenv("IPERF_SERVER_ADDRESS")
+PORT = os.getenv("IPERF_PORT")
+
 
 def main():
     load_dotenv()
+    print(f"Testing on host {HOST}, port {PORT}")
 
-    print (f"Testing on host {HOST}, port {PORT}")
+    command = [
+        "gstdbuf",
+        "-o0",
+        "iperf3",
+        "-c",
+        HOST,
+        "-p",
+        PORT,
+        "-i",
+        "0.5",
+        "-t",
+        "10",
+    ]
+
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True)
+
+    for line in process.stdout:
+        print(line.strip())
+
+
+main()
+
+
+"""
+DUMP
 
     iperf_client = Iperf_Client(HOST, PORT)
     iperf_client.set_duration(5)
@@ -20,7 +48,4 @@ def main():
     print (result)
 
     print ("YAY")
-
-
-
-main()
+"""
