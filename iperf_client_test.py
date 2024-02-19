8d24dca4804eba059e8db581510f810b7ebbf62e
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2024-02-19 04:57:11 -0500

    test working with intervals

--- iperf_client_test.py
+++ iperf_client_test.py
@@ -6,10 +6,10 @@ def main():
     iperf_client.server_hostname = "192.168.50.98"
     iperf_client.port = 5201
     iperf_client.duration = 6
-    iperf_client.test_reporter_interval = 2
+    iperf_client.test_reporter_interval = 1
     
-    iperf_client.test_stats_interval = 2
-    print(iperf_client)
+    iperf_client.test_stats_interval = 1
+    # print(iperf_client)
     
     result = iperf_client.run()
     print(result)
