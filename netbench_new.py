64cfcb2bc2cc00e2475370ba984c00d953b71131
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2024-02-20 01:06:15 -0500

    got bits per second parsing working

--- netbench_new.py
+++ netbench_new.py
@@ -11,23 +11,8 @@ import sys
 from multiprocessing import Pipe
 import os
 import select
+import json
 
-# Redirect stdout to our own stream.
-class MyStream:
-    def write(self, text):
-        # This is where we can parse the output and do what we need with it.
-        # print(f"Intercepted text: {text}", end="")
-        # print(f"Intercepted text: {text}", end="", flush=True)
-        message = "BUTTES"
-        sys.__stdout__.write(message)
-        # num_bytes = float(text["data"]["streams"]["bytes"], 0.0)
-        # sys.__stdout__.write(f"djdsklfjdskf {num_bytes}")
-        # dict(x=[data_x], y=[data_y])
-    # def flush(self):
-        # # Redirect to the original standard output
-        # return sys.__stdout__.flush()
-
-# sys.stdout = MyStream()
 
 class NetBench(Client):
     def __init__(self):
@@ -47,32 +32,52 @@ class NetBench(Client):
         self.worker_thread = Thread(
             target=self.run)
         self.worker_thread.start()
-        self.force_print("BUTTTESS")
         self.pipe_reader()
         # self.worker_thread.join()
         # redirect pipe_in to stdout for visibility
-        os.dup2(self._stdout_fd, 1)
+        # os.dup2(self._stdout_fd, 1)
         # os.dup2(stderr_fd, 2) # stdout
 
-        print("ENDND")
-        print(self.output) 
+        # print("ENDND")
+        # print(self.output) 
 
     def pipe_reader(self):
-        self.force_print(f"WORKER THREAD ALIVE? {self.worker_thread.is_alive()}") 
-        self.force_print(f"MORE DATA? {self.more_data()}")
         while self.worker_thread.is_alive():
             try:
                 msg = b""
                 # while self.more_data():
                 msg = os.read(self._pipe_out, 1024) 
-                msg = msg.decode('utf-8')
-                self.output.append(msg)
-                self.force_print(f"{msg}")
+                if msg:
+                    msg = msg.decode('utf-8')
+                    self.output.append(msg)
+                    self.parse_pipe_data(msg)
+                else:
+                    self.force_print("EMPTY MSG")
             except:
                 self.force_print("ERROR")
                 break
         self.force_print("LOOP DEAD")
         self.input_data_pipe.close() 
+
+    def parse_pipe_data(self, data):
+        try:
+            data_dict = json.loads(data)
+        except:
+            data_dict = {}
+            # self.force_print("bad json")
+
+        
+        try:
+            data = data_dict.get("data", {})
+            packet_sums = data.get("sum", {})
+            # packet_info = streams[0] if len(streams) > 0 else {}
+            if packet_sums:
+                bits_per_second = packet_sums.get("bits_per_second", 0.0)
+                print_string = str(bits_per_second)
+                self.force_print(print_string)
+        except Exception as e:
+            self.force_print(f"BAD THING {e}")
+
 
     def more_data(self):
         """Check if there is more data left on the pipe
@@ -84,7 +89,19 @@ class NetBench(Client):
         return bool(r)
 
     def force_print(self, message):
+        message = message + "\n"
         os.write(self._stdout_fd, message.encode())
+
+    def get_type_string(self, input):
+        try:
+            msg_type = type(data_dict).__name__ if type(data_dict) else ""
+            self.force_print(f"{msg_type}\n")                    
+        except Exception as e:
+            # print(f"Buttes {e}")
+            pass
+
+
+
 
 def main():
     netbench = NetBench()
