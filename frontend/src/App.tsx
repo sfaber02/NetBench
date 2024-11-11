0c903d2e10b292fe32f0b8523521be650a76f800
Shawn <shawn.faber@onepeloton.com>
2024-11-11 14:06:35 -0500

    adjust graph settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,28 +15,28 @@ const App: React.FC = () => {
         line2.append(Date.now(), Math.random());
       };
 
-      const dataInterval = setInterval(updateData, 1000);
+      const dataInterval = setInterval(updateData, 10);
 
       const smoothie = new SmoothieChart({
         grid: {
           strokeStyle: 'rgb(125, 0, 0)',
           fillStyle: 'rgb(60, 0, 0)',
-          lineWidth: 1,
-          millisPerLine: 250,
-          verticalSections: 6
+          lineWidth: 10,
+          millisPerLine: 25,
+          verticalSections: 2
         }
       });
 
       smoothie.addTimeSeries(line1, {
         strokeStyle: 'rgb(0, 255, 0)',
         fillStyle: 'rgba(0, 255, 0, 0.4)',
-        lineWidth: 3
+        lineWidth: 1
       });
 
       smoothie.addTimeSeries(line2, {
         strokeStyle: 'rgb(255, 0, 255)',
         fillStyle: 'rgba(255, 0, 255, 0.3)',
-        lineWidth: 3
+        lineWidth: 1
       });
 
       smoothie.streamTo(canvasRef.current, 1000);
